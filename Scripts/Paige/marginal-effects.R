57e1c42cb46fee206d0367592d061de59840fc32
Paige Miller <paigemiller@ecoadmins-iMac-10.local>
2019-03-12 11:46:34 -0400

    plots from today and start of code for plotting model diagnostics

--- Scripts/Paige/marginal-effects.R
+++ Scripts/Paige/marginal-effects.R
@@ -1,6 +1,8 @@
 # visualize marginal effects
 library(cowplot)
 
+#### --- SIMPLE models of parasite richness on simple dataset --- #### 
+
 simple_me <- readRDS("./Data/JPEK/simple/simple_brm_me.RDS")
 plot(simple_me[[4]])
 
@@ -29,4 +31,77 @@ bot <- plot_grid(plotlist = list(close, nonclose, micro, macro),
            ncol=2, nrow=2)
 pdf("./Results/richness_me_plot.pdf") 
 plot_grid(allp, bot, nrow=2, labels=c("A"))
+dev.off()
+
+#### --- SIMPLE models of parasite proportions on simple dataset --- #### 
+
+simple_me_parastrans <- readRDS("./Data/JPEK/simple/simple_brm_parastrans_me.RDS")
+plot(simple_me_parastrans[[4]])
+
+simple_me_parastype <- readRDS("./Data/JPEK/simple/simple_brm_parastype_me.RDS")
+plot(simple_me_parastype[[4]])
+
+simpTrans <- simple_me_parastrans[[4]] + ylab("Odds close") + theme(legend.position = "none") 
+simpType <- simple_me_parastype[[4]] + ylab("Odds micro") + theme(legend.position = "none") 
+
+
+botProp <- plot_grid(plotlist = list(simpTrans, simpType), 
+                     #labels = c("B", "C", "D", "E"), 
+                     ncol=1, nrow=2)
+
+pdf("./Results/propSimple_me_plot.pdf") 
+plot_grid(allp, botProp, nrow=2
+          #, labels=c("A")
+          )
+dev.off()
+
+#### --- FULL models of parasite proportions on FULL dataset --- #### 
+
+full_me <- readRDS("./Data/JPEK/full/full_brm_me.RDS")
+plot(full_me[[8]])
+
+full_me_parastrans <- readRDS("./Data/JPEK/full/full_brm_parastrans_me.RDS")
+plot(full_me_parastrans[[8]])
+
+full_me_parastype <- readRDS("./Data/JPEK/full/full_brm_parastype_me.RDS")
+plot(full_me_parastype[[8]])
+
+allp <- full_me[[8]] + ylab("Richness")
+fullTrans <- full_me_parastrans[[8]] + ylab("Odds close") + theme(legend.position = "none") 
+fullType <- full_me_parastype[[8]] + ylab("Odds micro") + theme(legend.position = "none") 
+
+botFull <- plot_grid(plotlist = list(simpTrans, simpType), 
+                     #labels = c("B", "C", "D", "E"), 
+                     ncol=1, nrow=2)
+
+pdf("./Results/propFull_plot.pdf") 
+plot_grid(allp, botProp, nrow=2
+          #, labels=c("A")
+)
+dev.off()
+
+
+#### --- SIMPLE models of parasite proportions on FULL dataset --- #### 
+
+simple_me_f <- readRDS("./Data/JPEK/simple/simple_brm_me_fulldat.RDS")
+plot(simple_me_f[[4]])
+
+simple_me_f_parastrans <- readRDS("./Data/JPEK/simple/simple_brm_parastrans_me_fulldat.RDS")
+plot(simple_me_f_parastrans[[4]])
+
+simple_me_f_parastype <- readRDS("./Data/JPEK/simple/simple_brm_parastype_me_fulldat.RDS")
+plot(simple_me_f_parastype[[4]])
+
+allp <- simple_me_f[[4]] + ylab("Richness")
+simpleTransFull <- simple_me_f_parastrans[[4]] + ylab("Odds close") + theme(legend.position = "none") 
+simpleTypeFull <- simple_me_f_parastype[[4]] + ylab("Odds micro") + theme(legend.position = "none") 
+
+botSimple_fulldat <- plot_grid(plotlist = list(simpleTransFull, simpleTypeFull), 
+                     #labels = c("B", "C", "D", "E"), 
+                     ncol=1, nrow=2)
+
+pdf("./Results/propsimple_fulldat_plot.pdf") 
+plot_grid(allp, botSimple_fulldat, nrow=2
+          #, labels=c("A")
+)
 dev.off()
---
+++ Scripts/Paige/model-diagnostics.R
@@ -0,0 +1,50 @@
+# Model diagnostics
+library(magrittr)
+library(cowplot)
+library(tidyverse)
+library(rstan)
+library(brms)
+library(broom)
+library(tidybayes)
+library(purrr)
+
+# Load models 
+
+# Richness
+simpleBrm <- readRDS("./Data/JPEK/simple/simple_brm_all.RDS") 
+simpleBrm_fulldat <- readRDS("./Data/JPEK/simple/simple_brm_all_fulldat.RDS") 
+fullBrm <- readRDS("./Data/JPEK/full/full_brm_all.RDS") 
+
+# Parasite transmission 
+simpleBrm_parastrans <- readRDS("./Data/JPEK/simple/simple_brm_parastrans.RDS") 
+simpleBrm_parastrans_fulldat <- readRDS("./Data/JPEK/simple/simple_brm_parastrans_fulldat.RDS") 
+fullBrm_parastrans <- readRDS("./Data/JPEK/full/full_brm_parastrans.RDS") 
+
+# Parasite type
+simpleBrm_parastype <- readRDS("./Data/JPEK/simple/simple_brm_parastype.RDS") 
+simpleBrm_parastype_fulldat <- readRDS("./Data/JPEK/simple/simple_brm_parastype_fulldat.RDS") 
+fullBrm_parastype <- readRDS("./Data/JPEK/full/full_brm_parastype.RDS") 
+
+########### RICHNESS #############
+
+### --- fit of simple model of richness --- ###
+
+### --- fit of full model of richness --- ###
+
+### --- fit of simple model of richness (on full dataset) --- ###
+
+########### PROP CLOSE #############
+
+### --- fit of simple model of prop close --- ###
+
+### --- fit of full model of prop close --- ###
+
+### --- fit of simple model of prop close (on full dataset) --- ###
+
+########### PROP MICRO #############
+
+### --- fit of simple model of prop micro --- ###
+
+### --- fit of full model of prop micro --- ###
+
+### --- fit of simple model of prop micro (on full dataset) --- ###
